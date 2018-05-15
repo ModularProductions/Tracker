67764ac35670c256c0e04e956a060320c6d4941e
Matt <777mth@gmail.com>
2018-05-15 01:10:19 -0400

    moves game state into its own object

--- client/src/Functions/canSee.js
+++ client/src/Functions/canSee.js
@@ -1,6 +1,7 @@
 
 // checks if light source is present in room and player is not blind, returns boolean
 export default function canSee(room, playerInv, creaturesPresent, modifiers) {
+  console.log("room =", room);
   let light = !room.dark;
   playerInv.forEach(ele => {
     if (ele.lightSource) light = true;
--- client/src/Functions/updateState.js
+++ client/src/Functions/updateState.js
@@ -5,21 +5,17 @@ export default function updateState (prevState, props, currData) {
     ...prevState,
     userCommand: "",
   };
-
-  if (currData.takesTime) {
-    newState.moveCount = prevState.moveCount++;
-  };
   
   // take in new relays
-  let oldRelay = prevState.relay;
+  let oldRelay = prevState.game.relay;
   currData.relay.forEach(ele => {
     // keep relay limited to 100 items
     if (oldRelay.length > 100) oldRelay.splice(0, 1);
     oldRelay.push(ele);       
   });
-  newState.relay = oldRelay;
+  newState.game.relay = oldRelay;
 
   // update other state changes
-  newState = mergeObjects(newState, currData.state);
+  newState.game = mergeObjects(newState.game, currData.state);
   return newState;
 };
