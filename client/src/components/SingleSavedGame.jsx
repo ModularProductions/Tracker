abac8e92db861e2030c38de92989a17c59b953c8
Matt <777mth@gmail.com>
2018-07-18 00:04:30 -0400

    temporary save game styling

--- client/src/components/SingleSavedGame.jsx
+++ client/src/components/SingleSavedGame.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Moment from "react-moment";
 
 const SingleSavedGame = (props) => (
-  <div>
-    <p className="savedGameName"><strong>First Game</strong></p>
-    <p>Location: {props.gameData.playerLocation}</p>
-    <p>Moves: {props.gameData.moveCount}, Saved at: <Moment format="hh:mma MMM Do, 'YY">{props.dateSaved}</Moment>.</p>
+  <li>
+    <div className="savedGameName"><strong>First Game</strong></div>
+    <div className="savedGameLocation">Location: {props.gameData.playerLocation}</div>
+    <div className="savedGameMoves">Moves: {props.gameData.moveCount}</div>
+    <div className="savedGameTime">Saved at: <Moment format="hh:mma MMM Do, 'YY">{props.dateSaved}</Moment></div>
     {/* {props.children} */}
-    <button onClick={() => props.handleDeleteButton(props.gameID)}>Delete</button>
-  </div>
+    <div className="deleteSaved" onClick={() => props.handleDeleteButton(props.gameID)}>Delete</div>
+  </li>
 );
-
 
 export default SingleSavedGame;
