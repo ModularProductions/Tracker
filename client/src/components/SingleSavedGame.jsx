3b1d77557c33a657ed411d0662ddadb564a9280b
Matt <777mth@gmail.com>
2018-08-15 15:58:57 -0400

    displays proper room name in saved games

--- client/src/components/SingleSavedGame.jsx
+++ client/src/components/SingleSavedGame.jsx
@@ -4,7 +4,7 @@ import Moment from "react-moment";
 const SingleSavedGame = (props) => (
   <li>
     <div className="savedGameName"><strong>First Game</strong></div>
-    <div className="savedGameLocation">Location: {props.gameData.playerLocation}</div>
+    <div className="savedGameLocation">Location: {props.currentPlayerLocation}</div>
     <div className="savedGameMoves">Moves: {props.gameData.moveCount}</div>
     <div className="savedGameTime">Saved at: <Moment format="hh:mma MMM Do, 'YY">{props.dateSaved}</Moment></div>
     {/* {props.children} */}
--- server/models/SaveState.js
+++ server/models/SaveState.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const saveStateSchema = new Schema({
   userID: { type: [String], required: true },
   dateSaved: { type: Date, default: Date.now },
+  currentPlayerLocation: { type: [String], required: true },
   quickSave: { type: Boolean, required: true },
   gameData: { type: {}, required: true },
 });
